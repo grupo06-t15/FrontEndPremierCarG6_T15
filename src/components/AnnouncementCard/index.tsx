a57860d4618f67f947a070325489a12889fbed8f
eduardo <eduardo.kucek@gmail.com>
2023-08-10 11:38:37 -0300

    feat: list cars

--- src/components/AnnouncementCard/index.tsx
+++ src/components/AnnouncementCard/index.tsx
@@ -18,25 +18,40 @@ export const AnnouncementCard = () => {
   return (
     <Announcement>
       <CarImage>
-        {/* {isActive ? <Active>Ativo</Active> : <Inactive>Inativo</Inactive>} */}
+        {isActive ? <Active>Ativo</Active> : <Inactive>Inativo</Inactive>}
         {isForSale && <Sale>$</Sale>}
         <img alt="Foto do carro" src="/src/assets/car.svg"></img>
       </CarImage>
-      <StyledText tag="h1" type="Heading-7-600" color="grey1">Marca & Modelo</StyledText>
-      <StyledText tag="p" type="Body-2-400" color="grey2">Descrição</StyledText>
+      <StyledText tag="h1" type="Heading-7-600" color="grey1">
+        Marca & Modelo
+      </StyledText>
+      <StyledText tag="p" type="Body-2-400" color="grey2">
+        Descrição
+      </StyledText>
       <UserData>
-      <StyledText tag="span" type="Body-2-500" color="brand1">EK</StyledText>
-        <StyledText tag="p" type="Body-2-500" color="grey2">Eduardo Kucek</StyledText>
+        <StyledText tag="span" type="Body-2-500" color="brand1">
+          EK
+        </StyledText>
+        <StyledText tag="p" type="Body-2-500" color="grey2">
+          Eduardo Kucek
+        </StyledText>
       </UserData>
       <CarData>
         <div>
-        <StyledText tag="span" type="Body-2-500" color="brand1">30.000 KM</StyledText>
-        <StyledText tag="span" type="Body-2-500" color="brand1">2023</StyledText>
+          <StyledText tag="span" type="Body-2-500" color="brand1">
+            30.000 KM
+          </StyledText>
+          <StyledText tag="span" type="Body-2-500" color="brand1">
+            2023
+          </StyledText>
         </div>
-        <StyledText tag="span" type="Heading-7-500" color="grey1"> {value.toLocaleString("pt-BR", {
+        <StyledText tag="span" type="Heading-7-500" color="grey1">
+          {" "}
+          {value.toLocaleString("pt-BR", {
             style: "currency",
             currency: "BRL",
-          })}</StyledText>
+          })}
+        </StyledText>
       </CarData>
     </Announcement>
   );

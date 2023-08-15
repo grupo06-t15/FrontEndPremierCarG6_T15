634447ae5f341c25321421c389744f170cc3aeb9
eduardo <eduardo.kucek@gmail.com>
2023-08-15 13:11:03 -0300

    feat: mobile menu

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,22 +1,59 @@
-import { NavContainer, StyledNavbar } from "./style";
+import { Menu, MobileMenu, NavContainer, StyledNavbar } from "./style";
 import NavbarLogo from "../../assets/premiercarnavbar.png";
 import { StyledText } from "../../styles/typography";
+import { useState } from "react";
 
 export const Navbar = () => {
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [buttonText, setButtonText] = useState("=");
+
+  const toggleMobileMenu = () => {
+    setMobileMenu((open) => !open);
+    if (!mobileMenu) {
+      setButtonText("x");
+    } else {
+      setButtonText("=");
+    }
+  };
+
   return (
     <StyledNavbar>
       <NavContainer>
         <img src={NavbarLogo} alt="Logo" />
-        <div>
-          <a href="/login">
-            <StyledText tag="p" type="Body-1-600" color="grey2">
-              Fazer Login
-            </StyledText>
-          </a>
-          <a href="/register">
-            <button>Cadastrar</button>
-          </a>
-        </div>
+        {mobileMenu ? (
+          <MobileMenu>
+            <li>
+              <a href="/login">
+                <StyledText tag="p" type="Body-1-600" color="grey2">
+                  Fazer Login
+                </StyledText>
+              </a>
+            </li>
+            <li>
+              <a href="/register">
+                <span>Cadastrar</span>
+              </a>
+            </li>
+          </MobileMenu>
+        ) : (
+          <Menu>
+            <li>
+              <a href="/login">
+                <StyledText tag="p" type="Body-1-600" color="grey2">
+                  Fazer Login
+                </StyledText>
+              </a>
+            </li>
+            <li>
+              <a href="/register">
+                <span>Cadastrar</span>
+              </a>
+            </li>
+          </Menu>
+        )}
+        <button type="button" onClick={toggleMobileMenu}>
+          {buttonText}
+        </button>
       </NavContainer>
     </StyledNavbar>
   );
--- src/components/Navbar/style.ts
+++ src/components/Navbar/style.ts
@@ -1,13 +1,14 @@
 import { styled } from "styled-components";
 
 export const StyledNavbar = styled.nav`
+  height: 80px;
   width: 100%;
 
   background-color: ${({ theme }) => theme.colors.grey10};
 
   border: solid 1px ${({ theme }) => theme.colors.grey6};
 
-  height: 80px;
+  z-index: 1;
 `;
 
 export const NavContainer = styled.div`
@@ -23,30 +24,86 @@ export const NavContainer = styled.div`
     width: 240px;
   }
 
-  div {
-    display: flex;
-    align-items: center;
-    gap: 44px;
-    padding-left: 44px;
+  button {
+    position: relative;
+    display: block;
+    height: 30px;
+    width: 30px;
+    padding: 6px;
+    border-radius: 6px;
+    font-size: 18px;
+  }
 
-    border-left: solid 1px ${({ theme }) => theme.colors.grey6};
-
-    height: 100%;
-
+  @media (min-width: 769px) {
     button {
-      color: ${({ theme }) => theme.colors.grey0};
-      background-color: ${({ theme }) => theme.colors.grey10};
-      border: solid 2px ${({ theme }) => theme.colors.grey4};
-
-      padding: 12px 28px;
-
-      border-radius: 4px;
-
-      font-family: Inter;
-      font-weight: 600;
-      size: 16px;
-
-      color: ${({ theme }) => theme.colors.grey2};
+      display: none;
     }
+  }
+`;
+
+export const Menu = styled.ul`
+  display: flex;
+
+  align-items: center;
+  gap: 44px;
+  padding-left: 44px;
+
+  border-left: solid 1px ${({ theme }) => theme.colors.grey6};
+
+  height: 100%;
+
+  span {
+    color: ${({ theme }) => theme.colors.grey0};
+    background-color: ${({ theme }) => theme.colors.grey10};
+    border: solid 2px ${({ theme }) => theme.colors.grey4};
+
+    padding: 12px 28px;
+
+    border-radius: 4px;
+
+    font-family: Inter;
+    font-weight: 600;
+    size: 16px;
+
+    color: ${({ theme }) => theme.colors.grey2};
+  }
+
+  @media (max-width: 769px) {
+    display: none;
+  }
+`;
+
+export const MobileMenu = styled.ul`
+  display: flex;
+  flex-direction: column;
+  margin-top: 0;
+  height: 100px;
+  width: 160px;
+  padding: 1rem;
+  gap: 1rem;
+
+  background-color: #ffffff;
+  border-radius: 6px;
+
+  span {
+    color: ${({ theme }) => theme.colors.grey0};
+    background-color: ${({ theme }) => theme.colors.grey10};
+    border: solid 2px ${({ theme }) => theme.colors.grey4};
+
+    padding: 4px 8px;
+
+    border-radius: 4px;
+
+    font-family: Inter;
+    font-weight: 600;
+    size: 12px;
+
+    color: ${({ theme }) => theme.colors.grey2};
+  }
+
+  @media (max-width: 769px) {
+    position: absolute;
+    margin-top: 3rem;
+    margin-left: 62%;
   }
 `;

ffd067a5f92997e19b613979a28c5a6613eb504e
NattanSilva <natanxboy@gmail.com>
2023-08-15 15:07:44 -0300

    Add: modal container and create announce modal

---
+++ src/components/ModalContainer/index.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from 'react';
+import { GeneralModalContainer } from './styled';
+
+interface IModalContainerProps {
+  children: ReactNode;
+}
+
+export const ModalContainer = ({ children }: IModalContainerProps) => {
+  return <GeneralModalContainer>{children}</GeneralModalContainer>;
+};
---
+++ src/providers/ModalProvider.tsx
@@ -0,0 +1,21 @@
+import { createContext, useState } from 'react';
+
+export interface IModalProviderProps {
+  children: React.ReactNode;
+}
+
+export interface IModalContext {
+  modalType: string;
+  setModalType: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export const ModalContext = createContext({} as IModalContext);
+
+export const ModalProvider = ({ children }: IModalProviderProps) => {
+  const [modalType, setModalType] = useState<string>('');
+  return (
+    <ModalContext.Provider value={{ modalType, setModalType }}>
+      {children}
+    </ModalContext.Provider>
+  );
+};

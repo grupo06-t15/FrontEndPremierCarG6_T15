ff8de1d004cca4d94707ab98622418be28287e03
eduardo <eduardo.kucek@gmail.com>
2023-08-24 17:24:39 -0300

    feat: resetPassword / user data in navbar

--- src/components/Form/LoginForm/index.tsx
+++ src/components/Form/LoginForm/index.tsx
@@ -4,79 +4,79 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useContext } from "react";
 import { UserContext } from "../../../providers/UserProvider";
 import {
-    ButtonsContainer,
-    EnterButton,
-    FieldsetContainer,
-    FormContainer,
-    StyledForm,
+  ButtonsContainer,
+  EnterButton,
+  FieldsetContainer,
+  FormContainer,
+  StyledForm,
 } from "../styled";
 import { Input } from "../Input";
 import { StyledText } from "../../../styles/typography";
 import { Link } from "react-router-dom";
 
 export const LoginForm = () => {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<LoginData>({
-        resolver: zodResolver(loginSchema),
-    });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginData>({
+    resolver: zodResolver(loginSchema),
+  });
 
-    const { userLogin } = useContext(UserContext);
+  const { userLogin } = useContext(UserContext);
 
-    const submit: SubmitHandler<LoginData> = (formData) => {
-        console.log("teste");
-        userLogin(formData);
-    };
+  const submit: SubmitHandler<LoginData> = (formData) => {
+    //console.log("teste");
+    userLogin(formData);
+  };
 
-    return (
-        <>
-            <StyledForm onSubmit={handleSubmit(submit)}>
-                <StyledText tag="h1" type="Heading-5-500" color="grey0">
-                    Login
-                </StyledText>
-                <FormContainer>
-                    <FieldsetContainer>
-                        <Input
-                            label="Email"
-                            placeholder="Digitar email"
-                            type="email"
-                            error={errors.email}
-                            register={register("email")}
-                        />
-                        <Input
-                            label="Senha"
-                            placeholder="Digitar senha"
-                            type="password"
-                            error={errors.password}
-                            register={register("password")}
-                        />
-                        <StyledText
-                            className="forgotPassword"
-                            tag="p"
-                            type="Body-2-500"
-                            color="grey2"
-                        >
-                            Esqueci minha senha
-                        </StyledText>
-                    </FieldsetContainer>
-                    <ButtonsContainer>
-                        <EnterButton type="submit">Entrar</EnterButton>
-                        <StyledText
-                            className="registerAcc"
-                            tag="p"
-                            type="Body-2-400"
-                            color="grey2"
-                        >
-                            Ainda não possui conta ?
-                        </StyledText>
-                        <Link className="registerButton" to="/register">
-                            Cadastrar
-                        </Link>
-                    </ButtonsContainer>
-                </FormContainer>
-            </StyledForm>
-        </>
-    );
+  return (
+    <>
+      <StyledForm onSubmit={handleSubmit(submit)}>
+        <StyledText tag="h1" type="Heading-5-500" color="grey0">
+          Login
+        </StyledText>
+        <FormContainer>
+          <FieldsetContainer>
+            <Input
+              label="Email"
+              placeholder="Digitar email"
+              type="email"
+              error={errors.email}
+              register={register("email")}
+            />
+            <Input
+              label="Senha"
+              placeholder="Digitar senha"
+              type="password"
+              error={errors.password}
+              register={register("password")}
+            />
+            <StyledText
+              className="forgotPassword"
+              tag="p"
+              type="Body-2-500"
+              color="grey2"
+            >
+              Esqueci minha senha
+            </StyledText>
+          </FieldsetContainer>
+          <ButtonsContainer>
+            <EnterButton type="submit">Entrar</EnterButton>
+            <StyledText
+              className="registerAcc"
+              tag="p"
+              type="Body-2-400"
+              color="grey2"
+            >
+              Ainda não possui conta ?
+            </StyledText>
+            <Link className="registerButton" to="/register">
+              Cadastrar
+            </Link>
+          </ButtonsContainer>
+        </FormContainer>
+      </StyledForm>
+    </>
+  );
 };
--- src/providers/UserProvider.tsx
+++ src/providers/UserProvider.tsx
@@ -1,9 +1,10 @@
 import { createContext, useState } from "react";
+
 import {
-    IDefaultProvidersProps,
-    IUserContextValues,
-    IUserLoginFormValues,
-    IUserRegisterFormValues,
+  IDefaultProvidersProps,
+  IUserContextValues,
+  IUserLoginFormValues,
+  IUserRegisterFormValues,
 } from "./@types";
 import { useNavigate } from "react-router-dom";
 import { api } from "../services/api";
@@ -12,63 +13,65 @@ import { toast } from "react-toastify";
 export const UserContext = createContext({} as IUserContextValues);
 
 export const UserProvider = ({ children }: IDefaultProvidersProps) => {
-    const [loading, setLoading] = useState(true);
-    const [user, setUser] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState([]);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const userLogin = async (formData: IUserLoginFormValues) => {
-        try {
-            const response = await api.post("/session", formData);
-            const { token } = response.data;
-            localStorage.setItem("@TOKEN", token);
-            api.defaults.headers.common.Authorization = `Bearer ${token}`;
-            console.log(token)
-            setUser(response.data);
-            navigate("/");
-            toast.success(`Bem Vindo ! `);
-        } catch (error) {
-            toast.error("Usuario ou senha invalido!");
-        }
-    };
+  const userLogin = async (formData: IUserLoginFormValues) => {
+    try {
+      const response = await api.post("/session", formData);
+      const { token } = response.data;
 
-    const userRegister = async (formData: IUserRegisterFormValues) => {
-        try {
-            setLoading(true);
-            const response = await api.post("/users", formData);
-            console.log("testei funCao register")
-            setUser(response.data);
-            toast.success(`Usuario cadastrado!!`);
-            navigate("/");
-        } catch (error: any) {
-            const errorMessage: string =
-                error.response?.data?.message ??
-                toast.error("Usuario não cadastrado!");
-            toast.error(`${errorMessage}!`);
-        } finally {
-            setLoading(false);
-        }
-    };
+      localStorage.setItem("@TOKEN", token);
+      api.defaults.headers.common.Authorization = `Bearer ${token}`;
+      //console.log(token);
+      //console.log(response);
 
-    const userLogout = () => {
-        setUser([]);
-        localStorage.removeItem("@TOKEN");
-        navigate("/");
-    };
+      setUser(response.data);
+      navigate("/");
+      toast.success(`Bem Vindo!`);
+    } catch (error) {
+      toast.error("Usuário ou senha inválido!");
+    }
+  };
 
-    return (
-        <UserContext.Provider
-            value={{
-                loading,
-                setLoading,
-                userLogin,
-                user,
-                setUser,
-                userRegister,
-                userLogout,
-            }}
-        >
-            {children}
-        </UserContext.Provider>
-    );
+  const userRegister = async (formData: IUserRegisterFormValues) => {
+    try {
+      setLoading(true);
+      const response = await api.post("/users", formData);
+      console.log("testei funCao register");
+      setUser(response.data);
+      toast.success(`Usuario cadastrado!!`);
+      navigate("/");
+    } catch (error: any) {
+      const errorMessage: string =
+        error.response?.data?.message ?? toast.error("Usuario não cadastrado!");
+      toast.error(`${errorMessage}!`);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const userLogout = () => {
+    setUser([]);
+    localStorage.removeItem("@TOKEN");
+    navigate("/");
+  };
+
+  return (
+    <UserContext.Provider
+      value={{
+        loading,
+        setLoading,
+        userLogin,
+        user,
+        setUser,
+        userRegister,
+        userLogout,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
 };

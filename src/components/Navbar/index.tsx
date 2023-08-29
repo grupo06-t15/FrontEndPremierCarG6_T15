1f7e3827dabf87138f9e53664803ebbb6cb00215
NattanSilva <natanxboy@gmail.com>
2023-08-29 17:08:53 -0300

    Add: user infos and navigate do user page

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -6,6 +6,7 @@ import { IUserRegisterFormValues } from '../../providers/@types';
 import { UserContext } from '../../providers/UserProvider';
 import { api } from '../../services/api';
 import { StyledText } from '../../styles/typography';
+import { UserInfos } from '../UserInfos';
 import { Menu, MobileMenu, NavContainer, StyledNavbar } from './style';
 
 export const Navbar = () => {
@@ -14,15 +15,14 @@ export const Navbar = () => {
   const [userData, setUserData] = useState<IUserRegisterFormValues | null>();
   const { setUser } = useContext(UserContext);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function loadUserData() {
       const token = localStorage.getItem('@TOKEN');
 
-
-            if (token) {
-                const decoded: string = jwt_decode(token!);
+      if (token) {
+        const decoded: string = jwt_decode(token!);
 
         const foundUser = await api.get(`/users/${decoded.sub!}`, {
           headers: {
@@ -37,7 +37,7 @@ export const Navbar = () => {
     }
     loadUserData();
   }, []);
-  console.log(userData);
+  // console.log(userData);
 
   const toggleMobileMenu = () => {
     setMobileMenu((open) => !open);
@@ -53,22 +53,27 @@ export const Navbar = () => {
       <NavContainer>
         <img src={NavbarLogo} alt="Logo" onClick={() => navigate('/')} />
         {mobileMenu ? (
-          <MobileMenu>
-            {/* {userData && <h1>{userData.name}</h1>} */}
-            <li>
-              <a href="/login">
-                <StyledText tag="p" type="Body-1-600" color="grey2">
-                  Fazer login
-                </StyledText>
-              </a>
-            </li>
-            <li>
-              <a href="/register">
-                <span>Cadastrar</span>
-              </a>
-            </li>
-          </MobileMenu>
-        ) : (
+          !localStorage.getItem('@TOKEN') ? (
+            <MobileMenu>
+              <li>
+                <a href="/login">
+                  <StyledText tag="p" type="Body-1-600" color="grey2">
+                    Fazer login
+                  </StyledText>
+                </a>
+              </li>
+              <li key={'register'}>
+                <a href="/register">
+                  <span>Cadastrar</span>
+                </a>
+              </li>
+            </MobileMenu>
+          ) : (
+            <MobileMenu>
+              <UserInfos userName={userData?.name} />
+            </MobileMenu>
+          )
+        ) : !localStorage.getItem('@TOKEN') ? (
           <Menu>
             {/* {userData && <h1>{userData.name}</h1>} */}
             <li>
@@ -83,6 +88,10 @@ export const Navbar = () => {
                 <span>Cadastrar</span>
               </a>
             </li>
+          </Menu>
+        ) : (
+          <Menu>
+            <UserInfos userName={userData?.name} />
           </Menu>
         )}
         <button type="button" onClick={toggleMobileMenu}>
